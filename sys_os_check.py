f692f0039d51d3b38ecb9f06823c39402a7e114b
fugasub9 <testfat5+gitsub1@gmail.com>
2021-01-10 16:35:55 +0900

    subtle modify for-linux-OS

--- sys_os_check.py
+++ sys_os_check.py
@@ -2,15 +2,18 @@ import sys
 import os
 import re
 
+tech_name = os.name
 system_os = sys.platform
+os_info = tech_name + " " + system_os
 user_home_path = os.environ['HOME']
 wsl2_path_pattern = "^\/mnt\/"
 #print(wsl2_path_pattern)
 
-print(system_os)
+#print(system_os)
+print(os_info)
 print(user_home_path)
 
-if(system_os == "linux"):
+if(system_os == "linux" or system_os == "linux2"):
 	if(re.match(wsl2_path_pattern, user_home_path)):
 		#print(re.match(wsl2_path_pattern, os.environ['HOME']))
 		print('wsl2 of Windows OS')
