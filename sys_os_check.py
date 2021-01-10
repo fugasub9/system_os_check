ddde74063ec56274bcf7b3b4083a7c47e629f681
fugasub9 <testfat5+gitsub1@gmail.com>
2021-01-10 16:50:59 +0900

    Erase some comment

--- sys_os_check.py
+++ sys_os_check.py
@@ -7,7 +7,6 @@ system_os = sys.platform
 os_info = tech_name + " " + system_os
 user_home_path = os.environ['HOME']
 wsl2_path_pattern = "^\/mnt\/"
-#print(wsl2_path_pattern)
 
 #print(system_os)
 print(os_info)
@@ -15,10 +14,8 @@ print(user_home_path)
 
 if(system_os == "linux" or system_os == "linux2"):
 	if(re.match(wsl2_path_pattern, user_home_path)):
-		#print(re.match(wsl2_path_pattern, os.environ['HOME']))
 		print('wsl2 of Windows OS')
 	else:
-		#print(re.match(wsl2_path_pattern, os.environ['HOME']))
 		print('other Linux OS')
 
 elif(system_os == "darwin"):
